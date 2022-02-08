98b3172d750620559852a7e90a80d820d3443a33
YiChenTsai <a0983936190@gmail.com>
2022-02-08 11:47:55 +0800

    切版sign-u.p.js

--- src/pages/Members/SignUp.js
+++ src/pages/Members/SignUp.js
@@ -7,49 +7,61 @@ function SignUp(showModal, setshowModal) {
   return (
     <>
       {showModal ? (
-        <div className="background">
-          <form className="d-flex justify-content-center align-items-centermt my-3">
-            <div style={{ width: '90%' }}>
+        <div className="background col-md-6 col-sm-12">
+          <form className="d-flex justify-content-center align-items-center my-3">
+            <div style={{ width: '60%' }}>
               <div className="d-flex justify-content-center">
-                <p className="title fs-1 fw-bold">Welcome to Join Us !</p>
+                <p className="title fs-2 fw-bold">Welcome to Join Us !</p>
               </div>
-              <div className="m-5">
-                <label>E-mail</label>
-                <input
-                  type="text"
-                  placeholder="Enter your email"
-                  className="form-control "
-                />
-              </div>
-              <div className="m-5">
-                <label>姓名</label>
-                <input
-                  type="text"
-                  placeholder="Enter your account"
-                  className="form-control"
-                />
-              </div>
-              <div className="m-5">
-                <label>密碼</label>
-                <input
-                  type="text"
-                  placeholder="Enter your password"
-                  className="form-control"
-                />
-              </div>
-              <div className="m-5">
-                <label
-                  className="form-check-label"
-                  type="checkbox"
-                  name="remember"
-                  id=""
-                />
-                我同意<span>服務條款</span>與<span>隱私政策</span>
+              <div className="d-flex justify-content-center">
+                <ul className="list-unstyled">
+                  <li className="mb-3">
+                    <label className="me-3">信箱</label>
+                    <input
+                      className="form-control mt-3 p-3"
+                      type="text"
+                      name="email"
+                      id=""
+                      placeholder="Email"
+                    />
+                  </li>
+                  <li className="mb-3">
+                    <label className="me-3">姓名</label>
+                    <input
+                      className="form-control mt-3 p-3"
+                      type="text"
+                      name="name"
+                      id=""
+                      placeholder="name"
+                    />
+                  </li>
+                  <li className="mb-3">
+                    <label className="me-3">密碼</label>
+                    <input
+                      className="form-control mt-3 p-3"
+                      type="text"
+                      name="password"
+                      id=""
+                      placeholder="password"
+                    />
+                  </li>
+                  <li className="d-flex justify-content-center m-4">
+                    <input
+                      className="form-check-input me-1"
+                      type="checkbox"
+                      name="agree"
+                      id=""
+                    />
+                    <label className="form-check-label">
+                      我同意<a>服務條款</a>與<a>隱私政策</a>
+                    </label>
+                  </li>
+                </ul>
               </div>
 
               <div className="d-flex justify-content-center">
                 <button
-                  className="btn btn-info text-white"
+                  className="btn btn-info text-white p-2"
                   style={{ width: '80px' }}
                 >
                   註冊
