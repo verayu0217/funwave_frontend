be2abea5d1caf8355830c184472848ba88139e27
goldsheep666 <wolf125tw100@gmail.com>
2022-02-08 22:48:42 +0800

    註冊彈窗done

--- src/pages/Members/SignUp.js
+++ src/pages/Members/SignUp.js
@@ -11,75 +11,73 @@ function SignUp(showModal, setshowModal) {
   });
   return (
     <>
-      {showModal ? (
-        <div className="background col-md-6 col-sm-4">
-          <form className="d-flex justify-content-center align-items-center my-3">
-            <div style={{ width: '60%' }}>
-              <div className="d-flex justify-content-center">
-                <p className="title fs-2 fw-bold">Welcome to Join Us !</p>
-              </div>
-              <div className="d-flex justify-content-center">
-                <ul className="list-unstyled">
-                  <li className="mb-3 signupInput">
-                    <label className="me-3">信箱</label>
-                    <input
-                      className="form-control mt-3 p-3 signupInput"
-                      type="text"
-                      name="email"
-                      id=""
-                      placeholder="Email"
-                      value={member.email}
-                    />
-                  </li>
-                  <li className="mb-3 signupInput">
-                    <label className="me-3">姓名</label>
-                    <input
-                      className="form-control mt-3 p-3 signupInput"
-                      type="text"
-                      name="name"
-                      id=""
-                      placeholder="name"
-                      value={member.name}
-                    />
-                  </li>
-                  <li className="mb-3 signupInput">
-                    <label className="me-3">密碼</label>
-                    <input
-                      className="form-control mt-3 p-3 signupInput"
-                      type="text"
-                      name="password"
-                      id=""
-                      placeholder="password"
-                      value={member.password}
-                    />
-                  </li>
-                  <li className="d-flex justify-content-center m-4 signupInput">
-                    <input
-                      className="form-check-input me-1"
-                      type="checkbox"
-                      name="agree"
-                      id=""
-                    />
-                    <label className="form-check-label agree">
-                      我同意<Link to="/">服務條款</Link>與
-                      <Link to="/">隱私政策</Link>
-                    </label>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="d-flex justify-content-center">
-                <button
-                  className="btn btn-info text-white p-2"
-                  style={{ width: '80px' }}
-                >
-                  註冊
-                </button>
-              </div>
+      <div className="background col-md-6 col-sm-4">
+        <form className="d-flex justify-content-center align-items-center my-3">
+          <div style={{ width: '60%' }}>
+            <div className="d-flex justify-content-center">
+              <p className="title fs-2 fw-bold">Welcome to Join Us !</p>
             </div>
-          </form>
-        </div>
-      ) : null}
+            <div className="d-flex justify-content-center">
+              <ul className="list-unstyled">
+                <li className="mb-3 signupInput">
+                  <label className="me-3">信箱</label>
+                  <input
+                    className="form-control mt-3 p-3 signupInput"
+                    type="text"
+                    name="email"
+                    id=""
+                    placeholder="Email"
+                    value={member.email}
+                  />
+                </li>
+                <li className="mb-3 signupInput">
+                  <label className="me-3">姓名</label>
+                  <input
+                    className="form-control mt-3 p-3 signupInput"
+                    type="text"
+                    name="name"
+                    id=""
+                    placeholder="name"
+                    value={member.name}
+                  />
+                </li>
+                <li className="mb-3 signupInput">
+                  <label className="me-3">密碼</label>
+                  <input
+                    className="form-control mt-3 p-3 signupInput"
+                    type="text"
+                    name="password"
+                    id=""
+                    placeholder="password"
+                    value={member.password}
+                  />
+                </li>
+                <li className="d-flex justify-content-center m-4 signupInput">
+                  <input
+                    className="form-check-input me-1"
+                    type="checkbox"
+                    name="agree"
+                    id=""
+                  />
+                  <label className="form-check-label agree">
+                    我同意<Link to="/">服務條款</Link>與
+                    <Link to="/">隱私政策</Link>
+                  </label>
+                </li>
+              </ul>
+            </div>
+
+            <div className="d-flex justify-content-center">
+              <button
+                className="btn btn-info text-white p-2"
+                style={{ width: '80px' }}
+              >
+                註冊
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
