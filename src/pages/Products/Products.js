06e80118d1cc88faa6b5091b52d3649a861bd4aa
Alice Yen <gp40424@gmail.com>
2022-02-09 23:51:12 +0800

    products

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import ProductBanner from '../../data/images/ProductBanner.jpg'; // 待釐清圖放src還是放在public
+import greenwave from '../../data/images/greenwave64x24.png'; // 待釐清圖放src還是放在public
 import './Products.scss';
 
 function Products(props) {
@@ -8,10 +8,10 @@ function Products(props) {
     <>
       {/* title、banner */}
       <div className="container-fluid banner">
-        <div className="d-flex justify-content-center align-items-end title">
+        <div className="d-flex justify-content-center align-items-end titleTop">
           <h4>FUN SHOP</h4>
         </div>
-        <div className="d-flex justify-content-center align-items-start">
+        <div className="d-flex justify-content-center align-items-start titleBottom">
           <h1>FUN 商店</h1>
         </div>
       </div>
@@ -19,7 +19,24 @@ function Products(props) {
       <div className="container">
         <h1>Breadcrumb</h1>
         <aside className="float-start">篩選</aside>
-        <article className="float-end">商品頁</article>
+        <article className="float-end">
+          <header className="position-relative">
+            <div className="d-flex justify-content-center">
+              <img src={greenwave} alt="greenwave" title="greenwave" />
+              <h1>短板</h1>
+            </div>
+            {/* 待搜尋icon */}
+            <input
+              value="Search "
+              className="form-control mt-1 rounded-pill search text-muted"
+              type="text"
+              name="search"
+              // onChange={(e) => setKeyword(e.target.value)}
+            />
+
+            {/* form-control會有會有自己的border-radius，好像蓋不掉，待研究！ */}
+          </header>
+        </article>
       </div>
     </>
   );
