0de7b6ce043cc7aebea83f79f5b3e7f45d64a827
YiChenTsai <a0983936190@gmail.com>
2022-02-12 21:15:34 +0800

    浪點切版update

---
+++ src/pages/SurfSpot/CircleRight.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './circleRight.scss';
+
+function CircleRight() {
+  return (
+    <div className="rightCirclePos">
+      <svg
+        id="圖層_1"
+        data-name="圖層 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-10 0 487.75 475.25"
+        className="rightCircle"
+      >
+        <defs></defs>
+        <circle className="cls-1 bgCircle" cx="250.13" cy="237.63" r="237.5" />
+        <circle className="cls-1 smCircle" cx="12.63" cy="237.63" r="12.5" />
+        <circle className="cls-1 smCircle" cx="141.09" cy="26.64" r="12.5" />
+        <circle className="cls-1 smCircle" cx="53.68" cy="105.34" r="12.5" />
+        <circle className="cls-1 smCircle" cx="53.68" cy="369.91" r="12.5" />
+        <circle className="cls-1 smCircle" cx="141.09" cy="448.79" r="12.5" />
+      </svg>
+      <text className="cls-2 h5 fw-bold EText text-nowrap">東部</text>
+      <text className="cls-2 h5 fw-bold ENText text-nowrap">東北部</text>
+      <text className="cls-2 h5 fw-bold NText text-nowrap">北部</text>
+      <text className="cls-2 h5 fw-bold WText text-nowrap">西部</text>
+      <text className="cls-2 h5 fw-bold SText text-nowrap">南部</text>
+    </div>
+  );
+}
+
+export default CircleRight;
--- src/pages/SurfSpot/SurfSpot.js
+++ src/pages/SurfSpot/SurfSpot.js
@@ -1,50 +1,33 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import greenTitle from '../../data/images/greenTitle.svg';
+import CircleLeft from './CircleLeft';
 import Taiwanmap from './TaiwanMap';
+import CircleRight from './CircleRight';
 import './surfSpot.scss';
 
 function SurfSpot() {
   return (
     <>
-      <div className="container-fluid taiwanMap">
+      <div className="container-fluid taiwanMapBg">
         <div className="container">
           <div className="wrapBread">麵包屑</div>
         </div>
 
         <div className="d-flex justify-content-center">
           <div className="text-center mt-3">
-            <img src="" alt="波浪icon" />
+            <img src={greenTitle} className="greenTitle" alt="greenTitle" />
             <div className="text-secondary fw-bold mt-3 h1">
               FUN 浪點 · 即時浪點資訊
             </div>
           </div>
         </div>
 
-        <div className="d-none d-md-block mt-5 mb-5">
+        <div className="mt-5 mb-5">
           <div className="d-flex justify-content-between">
-            <div className="circleLeft">
-              <div className="leftSpot1"></div>
-              <span className="basicSpot h5 fw-bold">初階浪點</span>
-              <div className="leftSpot2"></div>
-              <span className="mediumSpot h5 fw-bold">中階浪點</span>
-              <div className="leftSpot3"></div>
-              <span className="highSpot h5 fw-bold">高階浪點</span>
-            </div>
-            <div>
-              <Taiwanmap />
-            </div>
-            <div className="circleRight">
-              <div className="rightSpot1"></div>
-              <span className="eastSpot h5 fw-bold">東部</span>
-              <div className="rightSpot2"></div>
-              <span className="eastnorthSpot h5 fw-bold">東北部</span>
-              <div className="rightSpot3"></div>
-              <span className="northSpot h5 fw-bold">北部</span>
-              <div className="rightSpot4"></div>
-              <span className="middleSpot h5 fw-bold">中部</span>
-              <div className="rightSpot5"></div>
-              <span className="southSpot h5 fw-bold">南部</span>
-            </div>
+            <CircleLeft />
+            <Taiwanmap />
+            <CircleRight />
           </div>
         </div>
       </div>
