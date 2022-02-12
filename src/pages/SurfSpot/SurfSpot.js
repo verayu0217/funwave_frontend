48d1480ed9b853f454ed474819a0d63742aa7827
YiChenTsai <a0983936190@gmail.com>
2022-02-12 09:37:20 +0800

    浪點taiwanMap svg、切版

--- src/pages/SurfSpot/SurfSpot.js
+++ src/pages/SurfSpot/SurfSpot.js
@@ -1,24 +1,27 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import Taiwanmap from './TaiwanMap';
+import vector from '../../data/images/surfSpot/Vector.png';
+// import taiwanmap from '../../data/images/surfSpot/taiwanMap3.png';
 import './surfSpot.scss';
 
 function SurfSpot() {
   return (
     <>
       <div className="container-fluid taiwanMap">
-        <img src="" alt="底圖" />
         <div className="container">
           <div className="wrapBread">麵包屑</div>
         </div>
 
         <div className="d-flex justify-content-center">
           <div className="text-center mt-3">
-            <img src="" alt="波浪icon" />
+            <img src={vector} alt="波浪icon" />
             <div className="text-secondary fw-bold mt-3 h1">
               FUN 浪點 · 即時浪點資訊
             </div>
           </div>
         </div>
+
         <div className="d-none d-md-block mt-5 mb-5">
           <div className="d-flex justify-content-between">
             <div className="circleLeft">
@@ -30,7 +33,7 @@ function SurfSpot() {
               <span className="highSpot h5 fw-bold">高階浪點</span>
             </div>
             <div>
-              <img src="" alt="台灣地圖" />
+              <Taiwanmap />
             </div>
             <div className="circleRight">
               <div className="rightSpot1"></div>
