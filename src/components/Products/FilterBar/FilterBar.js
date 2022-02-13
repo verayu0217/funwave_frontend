cd0b0c0bbb356102c889c8f9b3eafe1f9f42b14b
Alice Yen <gp40424@gmail.com>
2022-02-13 15:54:31 +0800

    products filter-radio、check各一

--- src/components/Products/FilterBar/FilterBar.js
+++ src/components/Products/FilterBar/FilterBar.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PriceRangeRadio from './PriceRangeRadio';
-// import TagCheckbox from './TagCheckbox';
+import SizeCheckbox from './SizeCheckbox';
 
 // react-icons
-import { RiMoneyDollarCircleFill } from 'react-icons/ri';
+import { RiMoneyDollarCircleFill, RiRulerFill } from 'react-icons/ri';
 // import { FaThumbsUp } from 'react-icons/fa';
 // import { IoColorPalette } from 'react-icons/io5';
 // import { MdOutlineSurfing } from 'react-icons/md';
 
 function FilterBar(props) {
-  const { priceRangeTypes, priceRange, setPriceRange } = props;
+  const {
+    priceRangeTypes,
+    priceRange,
+    setPriceRange,
+    sizeTypes,
+    size,
+    setSize,
+  } = props;
+
+  const handleChecked = (e) => {
+    const value = e.target.value;
+    if (!size.includes(value)) return setSize([...size, value]);
+
+    if (size.includes(value)) {
+      const newTags = size.filter((v) => v !== value);
+      setSize(newTags);
+    }
+  };
 
   return (
     <>
@@ -18,13 +35,27 @@ function FilterBar(props) {
           <RiMoneyDollarCircleFill size={24} color="#17a8a2" className="me-3" />
           價格
         </h3>
-
         {priceRangeTypes.map((value, i) => (
           <PriceRangeRadio
             key={i}
             value={value}
             priceRange={priceRange}
             setPriceRange={setPriceRange}
+          />
+        ))}
+      </div>
+
+      <div className="mt-5">
+        <h3 className="d-flex align-items-center filterProducts pb-2 ps-3">
+          <RiRulerFill size={22} color="#17a8a2" className="me-3" />
+          尺寸
+        </h3>
+        {sizeTypes.map((value, i) => (
+          <SizeCheckbox
+            value={value}
+            key={i}
+            size={size}
+            handleChecked={handleChecked}
           />
         ))}
       </div>
