b8bef6572d50c8bc29ceb715d5b8ae410247e655
Vera Yu <verayu217@gmail.com>
2022-02-15 13:58:32 +0800

    部分重複拆成員元件引入

--- src/pages/Course/Courses.js
+++ src/pages/Course/Courses.js
@@ -2,47 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+// 引用上課地點組件
+import CourseSpot from './components/CourseSpot';
+
 import greenTitle from '../../data/images/greenTitle.svg';
 
 import './Courses.scss';
 
 function Courses() {
-  //先用假資料 先不從後端取
-  const [courseSpots, setCourseSpot] = useState([
-    {
-      id: 1,
-      name: '金沙',
-      image: 'https://picsum.photos/id/237/275/150',
-    },
-    {
-      id: 2,
-      name: '福隆海水浴場',
-      image: 'https://picsum.photos/id/237/275/150',
-    },
-    {
-      id: 3,
-      name: '頭城',
-      image: 'https://picsum.photos/id/237/275/150',
-    },
-    {
-      id: 4,
-      name: '白沙灣',
-      image: 'https://picsum.photos/id/237/275/150',
-    },
-  ]);
-
-  // useEffect(() => {
-  //   let getCourseSpot = async () => {
-  //     // http://localhost:3002/api/還沒取
-  //     let response = await axios.get(`${API_URL}/還沒取`);
-  //     setCourseSpot(response.data);
-  //   };
-  //   getCourseSpot();
-  // }, []);
-
   return (
     <>
-      {/* 主頁大圖區  */}
+      {/* 主頁大圖區加標題  */}
       <div className="container-fluid">
         <div className="row courseBanner-box">
           <div className="col-md-12 p-0 ">
@@ -54,13 +24,8 @@ function Courses() {
             </div>
           </div>
         </div>
-      </div>
-
-      {/* banner區標題 */}
-
-      {/* 課程簡介區  */}
-      <div className="container my-5">
-        <div className="row  course-box shadow-lg">
+        {/* 課程簡介區  */}
+        <div className="row col-10 m-auto course-box shadow-lg">
           <div className="text-secondary mt-5 h1 text-center mb-5">
             <img
               src={greenTitle}
@@ -111,67 +76,11 @@ function Courses() {
             </Link>
           </div>
         </div>
-      </div>
 
-      {/* 課程地點介紹區 */}
-      <div className="container my-5">
-        <div className="text-secondary  h1 text-center mb-5">
-          <img
-            src={greenTitle}
-            className="greenTitle me-3"
-            alt="greenTitle"
-            height="24px"
-            weight="64px"
-          />
-          優美的課程地點一覽
-        </div>
+        {/* 上課地點 */}
+        <CourseSpot />
 
-        <div className="row">
-          {/* 北部/東北部  */}
-          <p className="text-center h2 mb-3">北部/東北部</p>
-
-          {courseSpots.map((spot) => {
-            return (
-              <div key={spot.id} className="col-md-6 col-lg-3 ">
-                <div className="spot-img shadow-sm">
-                  <img src={spot.image} className="w-100 rounded" alt="..." />
-                  <p className="card-text text-center p-2">{spot.name}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
-        {/* 東部/西部/南部  */}
-        <div className="row">
-          <p className="text-center h2 mt-5 mb-3">東部/西部/南部</p>
-
-          {/* <div className="col-md-6 col-lg-3">
-            <div className="spot-img shadow-sm">
-              <img
-                src="https://fakeimg.pl/275x150/"
-                className="w-100 rounded"
-                alt="..."
-              />
-              <p className="card-text text-center p-2">白沙灣</p>
-            </div>
-          </div> */}
-
-          {courseSpots.map((spot) => {
-            return (
-              <div key={spot.id} className="col-md-6 col-lg-3 ">
-                <div className="spot-img shadow-sm">
-                  <img src={spot.image} className="w-100 rounded" alt="..." />
-                  <p className="card-text text-center p-2">{spot.name}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      {/* 教練介紹區 */}
-      <div className="container my-5">
+        {/* 教練介紹區 */}
         <div className="row">
           <div className="text-secondary  mt-3 h1 text-center mb-3">
             <img
