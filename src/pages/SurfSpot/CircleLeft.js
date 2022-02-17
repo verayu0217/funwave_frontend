9b327393b5afde6c02fe5abcb25b6a84494c9915
YiChenTsai <a0983936190@gmail.com>
2022-02-17 23:30:37 +0800

    text標籤更換span標籤

--- src/pages/SurfSpot/CircleLeft.js
+++ src/pages/SurfSpot/CircleLeft.js
@@ -17,9 +17,9 @@ function CircleLeft() {
         <circle className="cls-1 smCircle" cx="400.12" cy="200.02" r="12.5" />
         <circle className="cls-1 smCircle" cx="333.77" cy="348.91" r="12.5" />
       </svg>
-      <text className="cls-2 fw-bold bsText text-nowrap">初階浪點</text>
-      <text className="cls-2 fw-bold mdText text-nowrap">中階浪點</text>
-      <text className="cls-2 fw-bold hgText text-nowrap">高階浪點</text>
+      <span className="cls-2 fw-bold bsText text-nowrap">初階浪點</span>
+      <span className="cls-2 fw-bold mdText text-nowrap">中階浪點</span>
+      <span className="cls-2 fw-bold hgText text-nowrap">高階浪點</span>
     </div>
   );
 }
