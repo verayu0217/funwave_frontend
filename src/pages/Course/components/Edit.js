62dacc7371a338509001df5e66636b55d88de594
Vera Yu <verayu217@gmail.com>
2022-02-20 23:37:24 +0800

    增加留言功能

---
+++ src/pages/Course/components/Edit.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import moment from 'moment';
+
+// props拿出add
+const Edit = ({ add }) => {
+  const [msg, setMsg] = useState('');
+  function msgChange(e) {
+    setMsg(e.target.value);
+  }
+  // let now = moment().format('YYYY-MM-DD'); // 自動用今天的日期
+  // const [date, setDate] = useState(now);
+
+  // 綁定button的onClick方法
+  function addMsg() {
+    // 從父層傳下來的set方法
+    add(function (prevData) {
+      return [
+        ...prevData,
+        {
+          msg,
+        },
+      ];
+    });
+  }
+  // console.log(msg);
+  // console.log(now);
+
+  return (
+    <div className="d-flex   flex-column">
+      <label className="h3">評價留言</label>
+      <textarea
+        name=""
+        rows="4"
+        cols="50"
+        value={msg}
+        onChange={msgChange}
+      ></textarea>
+      <input
+        className="mt-2"
+        type="file"
+        id="photo"
+        name="photo"
+        onChange={(e) => {
+          // 圖片儲存的方式不太一樣
+          // setPhoto({ ...member, photo: e.target.files[0] });
+        }}
+      ></input>
+      <button onClick={addMsg} className="editBtn btn btn-primary mt-2">
+        送出留言
+      </button>
+    </div>
+  );
+};
+export default Edit;
