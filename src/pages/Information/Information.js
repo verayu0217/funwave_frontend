620ab885d49256ffb0cc40cec7fce036beeea336
monday-blue <tflying77@gmail.com>
2022-03-02 21:00:29 +0800

    加連結

--- src/pages/Information/Information.js
+++ src/pages/Information/Information.js
@@ -11,7 +11,7 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 import axios from 'axios';
 import { API_URL } from '../../utils/config';
 import { IMAGE_URL } from '../../utils/config';
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import Pagination from 'react-bootstrap/Pagination';
 import onClick from 'react-bootstrap/PageItem';
 import PageItem from 'react-bootstrap/PageItem';
@@ -386,39 +386,40 @@ function Information() {
                                 return (
                                 <React.Fragment key={allInfoItem.info_id}>
                                 <div className="col-12 col-lg-5 p-5" key={allInfoItem.info_no}>
-                            
+                                    <Link to={`/information/${allInfoItem.info_no}`} style={{textDecoration: 'none', color: '#333333'}}>
                                     {/* 卡片本體-中間 */}
-                                <div className="cardHome shadow">
-                                    {/* 卡片圖片容器-中間 */}
-                                    <div className="cardPicDivHome d-flex justify-content-center align-items-center">
-                                        {/* 卡片圖片標籤-中間 */}
-                                        <span class="badgeHome badge bg-dark h6">{allInfoItem.info_cat}</span>
-                                        {/* 卡片圖片本體-中間 */}
-                                        <img class="cardPicHome" src={`${IMAGE_URL}/information/${allInfoItem.big_img}`} alt={allInfoItem.big_img}/>
-                                    </div>
-                                    {/* 卡片文字容器-中間 */}
-                                    <div>
-                                        <div className="cardTitleHome px-4 pt-4 pb-4">
-                                            <h3>{allInfoItem.big_title}</h3>
+                                    <div className="cardHome shadow">
+                                        {/* 卡片圖片容器-中間 */}
+                                        <div className="cardPicDivHome d-flex justify-content-center align-items-center">
+                                            {/* 卡片圖片標籤-中間 */}
+                                            <span class="badgeHome badge bg-dark h6">{allInfoItem.info_cat}</span>
+                                            {/* 卡片圖片本體-中間 */}
+                                            <img class="cardPicHome" src={`${IMAGE_URL}/information/${allInfoItem.big_img}`} alt={allInfoItem.big_img}/>
                                         </div>
-                                        <div className="cardTextHome px-4 pt-4 pb-4">
-                                            <p>{allInfoItem.one_text_one}</p>
+                                        {/* 卡片文字容器-中間 */}
+                                        <div>
+                                            <div className="cardTitleHome px-4 pt-4 pb-4">
+                                                <h3>{allInfoItem.big_title}</h3>
+                                            </div>
+                                            <div className="cardTextHome px-4 pt-4 pb-4">
+                                                <p>{allInfoItem.one_text_one}</p>
+                                            </div>
+                                            <div className="cardinfoHome d-flex justify-content-between align-items-center px-4 pt-5 pb-4">
+                                                {/* 文章卡片按鈕 */}
+                                                <h6>{allInfoItem.create_time} {allInfoItem.author}</h6>
+                                                <div>
+                                                    <a className="btn btn-primary" href="" onClick={()=> navigate(`/information/${allInfoItem.info_no}`)}>看更多
+                                                    <FaLongArrowAltRight
+                                                            className="ps-2"
+                                                            size={24}
+                                                            color="#ffffff"
+                                                    />
+                                                    </a>
+                                                </div> {/* 文章卡片按鈕-結束 */}
+                                            </div>
                                         </div>
-                                        <div className="cardinfoHome d-flex justify-content-between align-items-center px-4 pt-5 pb-4">
-                                            {/* 文章卡片按鈕 */}
-                                            <h6>{allInfoItem.create_time} {allInfoItem.author}</h6>
-                                            <div>
-                                                <a className="btn btn-primary" href="" onClick={()=> navigate(`/information/${allInfoItem.info_no}`)}>看更多
-                                                <FaLongArrowAltRight
-                                                        className="ps-2"
-                                                        size={24}
-                                                        color="#ffffff"
-                                                />
-                                                </a>
-                                            </div> {/* 文章卡片按鈕-結束 */}
-                                        </div>
-                                    </div>
-                                </div> {/* 卡片本體-中間 cardHome */}
+                                    </div> {/* 卡片本體-中間 cardHome */}
+                                    </Link>
                                 </div> {/* column */}
                                 </React.Fragment>
                                 );
